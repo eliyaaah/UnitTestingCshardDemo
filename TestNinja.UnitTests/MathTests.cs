210e17a725e7282530771f8fc0704bb366f764ae
Illia Hladush <illia.gladush@gmail.com>
2021-02-06 17:49:20 -0500

    Adding tests for Math class

---
+++ TestNinja.UnitTests/MathTests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using System;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class MathTests
+    {
+        private Fundamentals.Math _math;
+
+        // SetUp
+        [SetUp]
+        public void SetUp()
+        {
+            _math = new Fundamentals.Math();
+        }
+
+        [Test]
+        public void Add_WhenCalled_ReturnSum()
+        {
+            var result = _math.Add(1, 2);
+
+            Assert.That(result, Is.EqualTo(3));
+        }
+
+        [Test]
+        [TestCase(2, 1, 2)]
+        [TestCase(1, 2, 2)]
+        [TestCase(1, 1, 1)]
+        public void Max_WhenCalled_ReturnTheGreaterArgument(int a, int b, int expectedResult)
+        {
+            var result = _math.Max(a, b);
+
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+    }
+}
